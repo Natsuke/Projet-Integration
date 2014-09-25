b567d31878f39e724515904db36e9d4a9d1bf314
seidh <nicolas.dasseler@gmail.com>
2014-09-25 09:35:08 +0200

    First great commit
    Enjoy it !

---
+++ application/views/vue.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>News du site internet</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Bonjour</h1>
+        <p>
+            Ceci est mon paragraphe !
+        </p>
+    </body>
+</html>
